9ecd07499027c4d5439b8b17a11fd6f29697bd46
Marli Frost <marli@frost.red>
2022-02-08 15:53:13 +0000

    replace strum dependency with manual impl

--- libcraft/core/src/block.rs
+++ libcraft/core/src/block.rs
@@ -3,228 +3,261 @@
 //! for actual block definitions.
 
 use serde::{Deserialize, Serialize};
-use strum_macros::EnumString;
+use std::str::FromStr;
 
-/// Direction a block is facing in.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-#[repr(u8)]
-pub enum BlockFace {
-    South,
-    SouthSouthwest,
-    Southwest,
-    WestSouthwest,
-    West,
-    WestNorthwest,
-    Northwest,
-    NorthNorthwest,
-    North,
-    NorthNortheast,
-    Northeast,
-    EastNortheast,
-    East,
-    EastSoutheast,
-    Southeast,
-    SouthSoutheast,
+
+macro_rules! impl_enum {
+    {
+        $(  
+            $(#[$doc:meta])*
+            $name:ident {
+                $($(#[$vdoc:meta])* $variants:ident),* $(,)?
+            }
+        )*
+    } => {
+        $(
+            $(#[$doc])*
+            #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+            pub enum $name {
+                $($(#[$vdoc])* $variants),*
+            }
+            impl FromStr for $name {
+                type Err = ();
+                fn from_str(s: &str) -> Result<Self, Self::Err> {
+                    #[allow(non_snake_case)]
+                    mod strings {
+                        $(
+                            pub mod $variants {
+                                const PASCAL_CASE: &[u8] = stringify!($variants).as_bytes();
+                                const LEN: usize = {
+                                    let mut len = PASCAL_CASE.len();
+                                    let mut i = 1;
+                                    while i < PASCAL_CASE.len() {
+                                        if PASCAL_CASE[i].is_ascii_uppercase() {
+                                            len += 1;
+                                        }
+                                        i += 1;
+                                    }
+                                    len
+                                };
+                                pub const SNAKE_CASE: &[u8] = &{
+                                    let mut buffer = [0; LEN];
+                                    buffer[0] = PASCAL_CASE[0].to_ascii_lowercase();
+                                    let mut i = 1;
+                                    let mut j = 1;
+                                    while i < buffer.len() {
+                                        let c = PASCAL_CASE[j];
+                                        if c.is_ascii_uppercase() {
+                                            buffer[i] = b'_';
+                                            i += 1;
+                                            buffer[i] = c.to_ascii_lowercase();
+                                        } else {
+                                            buffer[i] = c;
+                                        }
+                                        i += 1;
+                                        j += 1;
+                                    }
+                                    buffer
+                                };
+                            }
+                        )*
+                    }
+                    match s.as_bytes() {
+                        $(strings::$variants::SNAKE_CASE => Ok(Self::$variants),)*
+                        _ => Err(())
+                    }
+                }
+            }
+        )*
+    };
 }
 
-/// Size of bamboo leaves.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum BambooLeaves {
-    None,
-    Small,
-    Large,
+
+
+impl_enum! {
+    /// Direction a block is facing in.
+    #[repr(u8)]
+    BlockFace {
+        South,
+        SouthSouthwest,
+        Southwest,
+        WestSouthwest,
+        West,
+        WestNorthwest,
+        Northwest,
+        NorthNorthwest,
+        North,
+        NorthNortheast,
+        Northeast,
+        EastNortheast,
+        East,
+        EastSoutheast,
+        Southeast,
+        SouthSoutheast,
+    }
+
+    /// Size of bamboo leaves.
+    BambooLeaves {
+        None,
+        Small,
+        Large,
+    }
+    
+    /// Part of a bed.
+    BedPart {
+        Foot,
+        Head,
+    }
+    
+    /// How a bell is attached.
+    BellAttachment {
+        Ceiling,
+        Floor,
+        SingleWall,
+        DoubleWall,
+    }
+    
+    /// An axis. Used for bone blocks, portal blocks, chains, etc.
+    Axis {
+        X,
+        Y,
+        Z,
+    }
+    
+    /// Block face a button or grindstone is attached to.
+    AttachedFace {
+        Ceiling,
+        Floor,
+        Wall,
+    }
+    
+    /// Type of a chest.
+    ChestType {
+        Single,
+        /// Double chest; this block is on the left side.
+        Left,
+        /// Double chest; this block is on the right side.
+        Right,
+    }
+    /// Which half of a door or flower block is.
+    BlockHalf {
+        Lower,
+        Upper,
+    }
+    
+    /// Which half of stairs.
+    StairHalf {
+        Bottom,
+        Top,
+    }
+    
+    /// To which side a door's hinge is.
+    DoorHinge {
+        Left,
+        Right,
+    }
+    
+    /// Orientation of a jigsaw block.
+    Orientation {
+        DownEast,
+        DownNorth,
+        DownSouth,
+        DownWest,
+        EastUp,
+        NorthUp,
+        SouthUp,
+        UpEast,
+        UpNorth,
+        UpSouth,
+        UpWest,
+        WestUp,
+    }
+    
+    /// A note block instrument.
+    Instrument {
+        Banjo,
+        Basedrum,
+        Bass,
+        Bell,
+        Bit,
+        Chime,
+        CowBell,
+        Didgeridoo,
+        Flute,
+        Guitar,
+        Harp,
+        Hat,
+        IronXylophone,
+        Pling,
+        Snare,
+        Xylophone,
+    }
+    
+    /// Type of a slab block.
+    SlabType {
+        Bottom,
+        Top,
+        Double,
+    }
+    
+    /// Type of a moving piston or piston head.
+    PistonType {
+        Normal,
+        Sticky,
+    }
+    
+    /// Shape of a rail block.
+    RailShape {
+        EastWest,
+        NorthEast,
+        NorthSouth,
+        NorthWest,
+        SouthEast,
+        SouthWest,
+        AscendingEast,
+        AscendingNorth,
+        AscendingSouth,
+        AscendingWest,
+    }
+
+    /// Mode of a redstone comparator.
+    ComparatorMode {
+        Compare,
+        Subtract,
+    }
+
+    /// How a redstone dust connects to a given side.
+    RedstoneConnection {
+        None,
+        Side,
+        Up,
+    }
+
+    /// Shape of a stairs block.
+    StairShape {
+        InnerLeft,
+        InnerRight,
+        OuterLeft,
+        OuterRight,
+        Straight,
+    }
+    
+    /// Mode of a structure block.
+    StructureBlockMode {
+        Corner,
+        Data,
+        Load,
+        Save,
+    }
+    /// How a wall connects to a given direction.
+    WallConnection {
+        None,
+        Low,
+        Tall,
+    }
 }
 
-/// Part of a bed.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum BedPart {
-    Foot,
-    Head,
-}
-
-/// How a bell is attached.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum BellAttachment {
-    Ceiling,
-    Floor,
-    SingleWall,
-    DoubleWall,
-}
-
-/// An axis. Used for bone blocks,
-/// portal blocks, chains, etc.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum Axis {
-    X,
-    Y,
-    Z,
-}
-
-/// Block face a button or grindstone is attached to.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum AttachedFace {
-    Ceiling,
-    Floor,
-    Wall,
-}
-
-/// Type of a chest.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum ChestType {
-    Single,
-    /// Double chest; this block is on the left side.
-    Left,
-    /// Double chest; this block is on the right side.
-    Right,
-}
-
-/// Which half of a door or flower block is.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum BlockHalf {
-    Lower,
-    Upper,
-}
-
-/// Which half of stairs.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum StairHalf {
-    Bottom,
-    Top,
-}
-
-/// To which side a door's hinge is.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum DoorHinge {
-    Left,
-    Right,
-}
-
-/// Orientation of a jigsaw block.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum Orientation {
-    DownEast,
-    DownNorth,
-    DownSouth,
-    DownWest,
-    EastUp,
-    NorthUp,
-    SouthUp,
-    UpEast,
-    UpNorth,
-    UpSouth,
-    UpWest,
-    WestUp,
-}
-
-/// A note block instrument.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum Instrument {
-    Banjo,
-    Basedrum,
-    Bass,
-    Bell,
-    Bit,
-    Chime,
-    CowBell,
-    Didgeridoo,
-    Flute,
-    Guitar,
-    Harp,
-    Hat,
-    IronXylophone,
-    Pling,
-    Snare,
-    Xylophone,
-}
-
-/// Type of a slab block.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum SlabType {
-    Bottom,
-    Top,
-    Double,
-}
-
-/// Type of a moving piston or piston head.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum PistonType {
-    Normal,
-    Sticky,
-}
-
-/// Shape of a rail block.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum RailShape {
-    EastWest,
-    NorthEast,
-    NorthSouth,
-    NorthWest,
-    SouthEast,
-    SouthWest,
-    AscendingEast,
-    AscendingNorth,
-    AscendingSouth,
-    AscendingWest,
-}
-
-/// Mode of a redstone comparator.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum ComparatorMode {
-    Compare,
-    Subtract,
-}
-
-/// How a redstone dust connects to a given side.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum RedstoneConnection {
-    None,
-    Side,
-    Up,
-}
-
-/// Shape of a stairs block.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum StairShape {
-    InnerLeft,
-    InnerRight,
-    OuterLeft,
-    OuterRight,
-    Straight,
-}
-
-/// Mode of a structure block.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum StructureBlockMode {
-    Corner,
-    Data,
-    Load,
-    Save,
-}
-
-/// How a wall connects to a given direction.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, EnumString)]
-#[strum(serialize_all = "snake_case")]
-pub enum WallConnection {
-    None,
-    Low,
-    Tall,
+#[test]
+fn parses() {
+    assert_eq!("low".parse(), Ok(WallConnection::Low));
 }
